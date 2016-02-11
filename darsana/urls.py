1ff36ec079f87078dfdf7575b5ccbdbb4be485a5
sumesh <sumesh@qburst.com>
2016-02-11 12:42:21 +0530

    Create contact page for the site Add the following changes * Contact url to urlpatterns in urls.py * Contact view in views.py * Contact template to templates folder * Base template from which the templates extend.

--- darsana/urls.py
+++ darsana/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from . import views
 
 urlpatterns = [
     # Examples:
-    url(r'^$', 'darsana.views.home', name='home'),
+    url(r'^$', views.home, name='home'),
+    url(r'^contact/$', views.contact, name='contact'),
+    url(r'^sms/', include('sms.urls')),
     # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
 ]
